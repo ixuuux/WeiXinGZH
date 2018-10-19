8989f96e400e5348dff01151924cf1c2a6c7af44
Xu <ixuuux@qq.com>
2018-10-19 13:35:35 +0800

    Update main.py

--- GZH/main.py
+++ GZH/main.py
@@ -1,6 +1,7 @@
 import itchatmp
 import cy
 import random_num
+import qianming
 
 itchatmp.update_config(itchatmp.WechatConfig(
     token='your token',
@@ -9,12 +10,15 @@ itchatmp.update_config(itchatmp.WechatConfig(
 
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):
-    if msg['Content'] == '帮助'
+    if msg['Content'] == '帮助':
         return '帮助信息'
     
     elif re.search('[0-9]', msg['Content'][0]) and re.search('(-)', msg['Content'][:]) and re.search('[0-9]', msg['Content'][-1]):
         num = msg.split('-')
         return str(random_num.random_number(int(num[0]), int(num[-1])))
+    
+    elif msg['Content'][0] in ['q', 'Q'] and len(msg['Content']) > 1:
+        return qianming.qm(msg['Content'][1:])
     
     else:
         key = msg['Content'][-1]
